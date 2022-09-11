b654aa6830a84665ebdcc2ecbc059afe6d958757
Dimitri Ligdamis <ligdamisdimitri@gmail.com>
2022-09-11 23:53:05 +0200

    :gear: config: Setup project

---
+++ src/App/App.js
@@ -0,0 +1,12 @@
+import CardNft from '../components/CardNft';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <CardNft />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CardNft/index.js
@@ -0,0 +1,9 @@
+import './style.scss';
+
+export default function CardNft() {
+  return (
+    <div className='CardNft'>
+      
+    </div>
+  )
+}
